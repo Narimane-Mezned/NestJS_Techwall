12f5847d92cb96e5e68f8b982cc50b93e45ffec9
Narimane-Mezned <nerimenemezned24@gmail.com>
2026-05-05 23:55:55 +0100

    Controllers : update , create , delete a todo

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,31 +1,77 @@
-import { Controller, Delete, Get, Post, Req, Res } from '@nestjs/common';
-import type { Request, Response } from 'express';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Put,
+  Param,
+  NotFoundException,
+  Query,
+} from '@nestjs/common';
+import { Todo } from './entities/todo.entity';
 @Controller('todo')
 export class TodoController {
-  @Get()
-  getTodos() {
-    console.log('recuperer la liste des todos');
-    return 'liste des todos';
+  todos: Todo[];
+  constructor() {
+    this.todos = [];
   }
-  @Get('version2')
-  getTodos_V2(@Req() request: Request, @Res() response: Response) {
-    //console.log(request);
-    console.log('recuperer la liste des todos');
-    //console.log(response);
-    response.status(205);
-    response.json({
-      contenu:
-        'je suis une réponse générée à partir de l objet Response de express',
-    });
+  @Get()
+  getTodos(@Query() mesQueryParams) {
+    console.log(mesQueryParams);
+    return this.todos;
+  }
+  @Get('/:id')
+  getTodoById(@Param('id') id) {
+    const todo = this.todos.find((t) => t.id === +id); //+id pour convertir id en nombre
+    console.log('recuperer un todo par id');
+    if (todo) return todo;
+    throw new NotFoundException("le todo avec l'id" + id + " n'existe pas");
   }
   @Post()
-  addTodo() {
-    console.log('ajouter un todo');
-    return 'todo ajouté';
+  //je vais pas récupérer l'id du body mais plutot l'incrémenter et l'ajouter au tableau todos
+  addTodo(@Body() newTodo: Todo) {
+    if (this.todos.length) {
+      newTodo.id = this.todos[this.todos.length - 1].id + 1; //dernier id dans le tableau + 1 pour l'incrémenter
+    } else {
+      newTodo.id = 1; //si le tableau est vide, le premier id sera 1
+    }
+    this.todos.push(newTodo);
+    return newTodo;
   }
-  @Delete()
-  deleteTodo() {
-    console.log('Supprimer un todo');
-    return 'todo supprimé';
+
+  //suppression d'un todo par id
+  @Delete('/:id')
+  deleteTodoByID(@Param('id') id) {
+    //chercher l'objet via son id
+    const index = this.todos.findIndex((t) => t.id === +id);
+    // utiliser la méthode splice pour la suppression si l'élément existe
+    if (index >= 0) {
+      this.todos.splice(index, 1); //supprimer l'élément à l'index trouvé , 1 pour supprimer un seul élément
+    } else {
+      // si l'élément n'est pas trouvé (index=-1) , on lance une erreur 404
+      throw new NotFoundException(`le todo d'id ${id} n'est pas trouvé `);
+    }
+    return `le todo d'id ${id} a été supprimé avec succès`;
+  }
+
+  @Put('/:id')
+  modifierTodo(@Param('id') id, @Body() newtodo: Partial<Todo>) {
+    const todo = this.getTodoById(id); //récupérer le todo à modifier
+    /*
+    todo ={
+      ...todo ,
+      ...newtodo} // ce syntaxe veut dire récupérer tout ce qu'on a dans le todo et dans le newtodo
+    en faisant ça , on modifie la référence d'un objet , donc cet objet en question ne va pas etre modifier  */
+
+    //pour éviter ce problème , on va modifier les propriétés de l'objet todo directement :
+    todo.name = newtodo.name ? newtodo.name : todo.name;
+    todo.description = newtodo.description
+      ? newtodo.description
+      : todo.description;
+
+    return 'todo modifié avec succès';
   }
 }
+// partial<todo> : pour dire que les propriétés de l'objet todo sont optionnelles
+// on peut modifier une partie des propriétés du todo sans être obligé de fournir toutes les propriétés lors de la modification
